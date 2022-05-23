4c231a40b17b63b15a3a945fe3815e39c215ee58
PERSONAL <glopezvan@gmail.com>
2022-05-23 16:30:58 -0500

    Ejercicio 1 S4 condicionales

---
+++ Ejercicio1_S4.py
@@ -0,0 +1,7 @@
+
+edadUsuario = int(input('Ingrese su edad: '))
+
+if edadUsuario >= 18 :
+    print(f'Usted es mayor de edad. edad:{edadUsuario}')
+else:
+    print(f'Usted es menor de edad. Edad:{edadUsuario}')
